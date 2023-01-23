0097f90ef642af1cf01d46500f75b249b995dc23
ngntanloc <nguyentanloc160201@gmail.com>
2023-01-23 14:41:28 +0700

    sqlite in flutter

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenExceoption implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class UserAlreadyExisted implements Exception {}
+
+class CoundNotFindUser implements Exception {}
+
+class CoundNotFindNote implements Exception {}
+
+class CoundNotUpdateNote implements Exception {}
